a2b72e13b98405cb52f76f5c98b6bbb05287155d
Eragon816 <105925973+Eragon816@users.noreply.github.com>
2025-08-03 20:09:39 +0200

    Update groups.js

--- groups.js
+++ groups.js
@@ -1,35 +1,36 @@
 // groups.js - Definition der festen Spielgruppen
 
 // NEU: "export" hinzugefügt, damit andere Module diese Variable importieren können.
-export const gameGroups = [
-  {
-    id: 1,
-    name: "Labubu",
-    code: "Host123",
-    color: "#8e44ad", // Lila
+export const gameGroups = [Lila
   },
   {
-    id: 2,
+    id: 1,
     name: "Die Schlümpfe",
     code: "Blabla",
     color: "#2980b9", // Rot
   },
   {
-    id: 3,
+    id: 2,
     name: "Yeeeeeee team",
     code: "Ahhahha",
     color: "#27ae60", // Grün
   },
   {
-    id: 4,
+    id: 3,
     name: "Die Sterne",
     code: "123456b",
     color: "#a8a4a4ff", // Blau
   },
   {
-    id: 5,
+    id: 4,
     name: "Player 456",
     code: "Sadness",
     color: "#f39c12", // Orange
   },
+  
+  {
+    id: 5,
+    name: "Labubu",
+    code: "Host123",
+    color: "#8e44ad", // 
 ];
